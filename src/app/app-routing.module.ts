552ee6b759ef37eeccccf7aa2f756876d468960d
Ahmed-lasmar <91993395+Ahmed-lasmar@users.noreply.github.com>
2023-02-10 14:57:56 +0100

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {RouterModule, Routes} from "@angular/router";
+import {ProductsComponent} from "./products/products.component";
+import {OffresEmploiComponent} from "./offres-emploi/offres-emploi.component";
 
+const routes: Routes =[
+  {path:'',redirectTo:'products',pathMatch:'full'},
+  {path:'products',component:ProductsComponent},
+  {path:'offres',component:OffresEmploiComponent}
 
+]
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    RouterModule.forRoot(routes)
+  ],
+  exports:[RouterModule]
 })
 export class AppRoutingModule { }
